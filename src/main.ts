e1882b8e0e7f481d333dcc1c349d8d2da6d2cc49
Maksym84 <burimskiy84@gmail.com>
2023-06-06 21:29:05 +0300

    app.enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './blog/dto/create-blog.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // номер порту змінено
   await app.listen(3100);
